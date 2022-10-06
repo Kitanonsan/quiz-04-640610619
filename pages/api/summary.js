6fb37deec3d2be70d719cc79f8141d5738b36323
Kitanonsan <Kitanon_san@cmu.ac.th>
2022-10-06 15:10:08 +0700

    quiz4

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,30 @@
+import { readUsersDB, writeUsersDB } from "../../backendLibs/dbLib";
+import { checkToken } from "../../backendLibs/checkToken";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || user.isAdmin !== true) {
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
     //compute DB summary
+    let userCount = 0;
+    let adminCount = 0;
+    let totalMoney = 0;
+    const users = readUsersDB().map((x) => {
+      if (x.isAdmin === true) {
+        adminCount = adminCount + 1;
+      } else {
+        userCount = userCount + 1;
+        totalMoney = totalMoney + x.money;
+      }
+    });
+
     //return response
+    return res
+      .status(200)
+      .json({ ok: true, userCount, adminCount, totalMoney });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -1,7 +1,16 @@
+import { readUsersDB, writeUsersDB } from "../../backendLibs/dbLib";
+import { checkToken } from "../../backendLibs/checkToken";
+
 export default function withdrawRoute(req, res) {
   if (req.method === "PUT") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
+
+    const user = checkToken(req);
+    if (!user || user.isAdmin === true) {
+      return res
+        .status(403)
+        .json({ ok: false, message: "You do not have permission to withdraw" });
+    }
 
     const amount = req.body.amount;
     //validate body
@@ -9,12 +18,25 @@ export default function withdrawRoute(req, res) {
       return res.status(400).json({ ok: false, message: "Invalid amount" });
 
     //check if amount < 1
-    // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
+    if (amount < 1) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Amount must be greater than 0" });
+    }
 
     //find and update money in DB (if user has enough money)
-    //return res.status(400).json({ ok: false, message: "You do not has enough money" });
+    const users = readUsersDB();
+    const userIdx = users.findIndex((x) => x.username === user.username);
+    if (users[userIdx].money - amount < 0) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "You do not has enough money" });
+    }
 
+    users[userIdx].money = users[userIdx].money - amount;
+    writeUsersDB(users);
     //return response
+    return res.status(200).json({ ok: true, money: users[userIdx].money });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
